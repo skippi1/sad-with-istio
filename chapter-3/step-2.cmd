e49a1e980953690f2c02047b534f4a1d2382bf4b
Markus Breuer <markus.breuer@gmx.de>
2021-02-01 22:05:56 +0100

    chapter 3 finished

--- chapter-3/step-2.cmd
+++ chapter-3/step-2.cmd
@@ -1,1 +1,7 @@
 kubectl.exe apply -n online-boutique -f frontend-gateway.yaml
+
+curl.exe -H "Host: marketplace.boutiquestore.com" --cacert root.crt --resolve "marketplace.boutiquestore.com:443:127.0.0.1" "https://marketplace.boutiquestore.com:443/_healthz"
+
+openssl.exe s_client -showcerts -connect 127.0.0.1:56776 -servername marketplace.boutiquestore.com -CAfile root.crt
+
+openssl.exe s_client -showcerts -connect 127.0.0.1:56776 -servername marketplace.boutiquestore.com -CAfile root.crt -debug
